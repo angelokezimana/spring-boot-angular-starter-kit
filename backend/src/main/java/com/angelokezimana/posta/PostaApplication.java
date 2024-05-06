7c8e58779b770f0d3519c9164572f043b7a32afa
Kezimana Aimé Angelo <kezangelo@gmail.com>
2024-05-06 15:25:06 +0200

    initial commit

---
+++ backend/src/main/java/com/angelokezimana/posta/PostaApplication.java
@@ -0,0 +1,13 @@
+package com.angelokezimana.posta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostaApplication.class, args);
+	}
+
+}
