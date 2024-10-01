5f0a27a960ae28a36f5a1ab46cf1f088c58ba140
Kezimana Aimé Angelo <kezangelo@gmail.com>
2024-10-01 15:47:02 +0200

    Made some design

--- src/main/webapp/src/app/pages/home/home.component.ts
+++ src/main/webapp/src/app/pages/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from "@angular/material/card";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {TruncatePipe} from "../../pipe/truncate.pipe";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatToolbarModule, MatButtonModule, TruncatePipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
