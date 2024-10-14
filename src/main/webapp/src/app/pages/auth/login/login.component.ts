b458017f7bf6275fa50d6770b1964187b9b369b3
Kezimana Aimé Angelo <kezangelo@gmail.com>
2024-10-14 12:07:24 +0200

    implement the form validation to the login form

--- src/main/webapp/src/app/pages/auth/login/login.component.ts
+++ src/main/webapp/src/app/pages/auth/login/login.component.ts
@@ -3,16 +3,18 @@ import {MatCardModule} from "@angular/material/card";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatIconModule} from "@angular/material/icon";
 import {MatInputModule} from "@angular/material/input";
-import {FormsModule} from "@angular/forms";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 import {MatButtonModule} from "@angular/material/button";
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {RouterModule} from "@angular/router";
 import {FooterComponent} from "../../../components/footer/footer.component";
+import {CommonModule} from "@angular/common";
+import {FormValidationService} from "../../../service/form-validation.service";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatCardModule, FormsModule, MatFormFieldModule, MatInputModule,
+  imports: [CommonModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule,
     MatButtonModule, MatIconModule, MatCheckboxModule, RouterModule, FooterComponent
   ],
   templateUrl: './login.component.html',
@@ -20,4 +22,19 @@ import {FooterComponent} from "../../../components/footer/footer.component";
 })
 export class LoginComponent {
 
+  loginFormGroup = this.formBuilder.group({
+    'email': ['', [Validators.required, Validators.email]],
+    'password': ['', Validators.required]
+  });
+
+  constructor(private formBuilder: FormBuilder, private formValidationService: FormValidationService) {
+  }
+
+  isFieldValid(name: string): boolean | undefined {
+    return this.formValidationService.isFieldValid(this.loginFormGroup, name);
+  }
+
+  login(): void {
+    console.log(this.loginFormGroup.value);
+  }
 }
