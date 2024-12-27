f0d5f88ee1d29968540b31a2d81eabcf361cbbbf
Kezimana Aime Angelo <kezangelo@gmail.com>
2024-12-27 17:09:37 +0200

    wip

--- src/main/java/com/angelokezimana/posta/service/security/PasswordResetService.java
+++ src/main/java/com/angelokezimana/posta/service/security/PasswordResetService.java
@@ -1,7 +1,6 @@
 package com.angelokezimana.posta.service.security;
 
 import com.angelokezimana.posta.dto.security.ResetPasswordRequestDTO;
-import jakarta.mail.MessagingException;
 
 public interface PasswordResetService {
     void generatePasswordResetToken(String email);
