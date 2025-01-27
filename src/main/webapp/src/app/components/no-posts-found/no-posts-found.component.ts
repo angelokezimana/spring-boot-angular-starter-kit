2cf1183e31209144318a41411c2228392f295062
Kezimana Aime Angelo <kezangelo@gmail.com>
2025-01-27 21:22:06 +0200

    wip

---
+++ src/main/webapp/src/app/components/no-posts-found/no-posts-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-posts-found',
+  standalone: true,
+  imports: [],
+  templateUrl: './no-posts-found.component.html',
+  styleUrl: './no-posts-found.component.scss'
+})
+export class NoPostsFoundComponent {
+
+}
--- src/main/webapp/src/app/services/post-service/post.service.ts
+++ src/main/webapp/src/app/services/post-service/post.service.ts
@@ -1,19 +1,27 @@
-import {Injectable} from '@angular/core';
+import {Injectable, signal} from '@angular/core';
 import {WebApiService} from '../web-api/web-api.service';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import Post from '../../models/blog/post.model';
 import PostDetail from "../../models/blog/post-detail.model";
 import {HttpResponse} from "@angular/common/http";
+import {tap} from "rxjs/operators";
+import {toObservable} from "@angular/core/rxjs-interop";
 
 @Injectable({
   providedIn: 'root',
 })
 export class PostService {
+  private loading = signal(false);
+
   constructor(private http: WebApiService) {
   }
 
   getPosts(): Observable<HttpResponse<Post[]>> {
-    return this.http.get('/api/v1/posts');
+    this.loading.set(true);
+
+    return this.http.get('/api/v1/posts').pipe(
+      tap(() => this.loading.set(false))
+    );
   }
 
   getPostById(id: number): Observable<HttpResponse<PostDetail>> {
@@ -26,5 +34,9 @@ export class PostService {
 
   updatePost(formData: FormData, id: number): Observable<any> {
     return this.http.put(`/api/v1/posts/${id}`, formData, true);
+  }
+
+  loadingStatus():Observable<boolean>{
+    return toObservable(this.loading);
   }
 }
