48dc48c3e9ee14e0e5f29b31d50acb48a41d0198
Kezimana Aime Angelo <kezangelo@gmail.com>
2025-02-05 11:06:13 +0200

    Custom filter to exclude byte[] fields

--- src/main/java/com/angelokezimana/posta/advice/LoggingAdvice.java
+++ src/main/java/com/angelokezimana/posta/advice/LoggingAdvice.java
@@ -1,7 +1,12 @@
 package com.angelokezimana.posta.advice;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
@@ -9,6 +14,8 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Aspect
 @Component
@@ -42,7 +49,20 @@ public class LoggingAdvice {
 
     private String safeToJson(Object obj) {
         try {
-            return objectMapper.writeValueAsString(obj);
+            ObjectMapper customMapper = objectMapper.copy();
+            customMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+
+            // Custom filter to exclude byte[] fields
+            SimpleModule module = new SimpleModule();
+            module.addSerializer(byte[].class, new JsonSerializer<byte[]>() {
+                @Override
+                public void serialize(byte[] value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+                    gen.writeString("[hidden]");
+                }
+            });
+            customMapper.registerModule(module);
+
+            return customMapper.writeValueAsString(obj);
         } catch (JsonProcessingException e) {
             logger.warn("Serialization failed for {}: {}",
                     obj.getClass().getSimpleName(), e.getMessage());
